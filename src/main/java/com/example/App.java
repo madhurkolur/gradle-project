92cd92aca4e1bd33de6fedaec446389f423412e1
madhurkolur <126852936+madhurkolur@users.noreply.github.com>
2026-03-27 11:00:54 +0530

    Update greeting message in App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Gradle Jenkins Demo Project22222!");
+        System.out.println("Hello from Gradle Jenkiiiiiiiiiiiiins Demo Project22222!");
     }
 }
