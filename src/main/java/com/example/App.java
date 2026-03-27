d2b8412fb522ccf74d42002e4d0967aa60c05501
madhurkolur <126852936+madhurkolur@users.noreply.github.com>
2026-03-27 10:57:20 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Gradle Jenkins Demo Project!");
+        System.out.println("Hello from Gradle Jenkins Demo Project22222!");
     }
 }
